a4e5e6e35ebc95dd066b20a532548d14ae4e63f0
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-06-22 19:36:40 +0300

    Implement Find Author By Name

--- src/main/java/kz/zhelezyaka/sdjpaJdbcTemplate/dao/AuthorDaoImpl.java
+++ src/main/java/kz/zhelezyaka/sdjpaJdbcTemplate/dao/AuthorDaoImpl.java
@@ -21,7 +21,8 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-        return null;
+        return jdbcTemplate.queryForObject("SELECT * FROM author WHERE first_name = ? and last_name = ?",
+                getRowMapper(), firstName, lastName);
     }
 
     @Override
